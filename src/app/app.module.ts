999aee1b18cd3eb1c2afb61fd1745b42137d1d8c
kevpdev <prepontkevin@gmail.com>
2019-10-07 16:46:21 +0200

    architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { UiModule } from './ui/ui.module';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule,
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
