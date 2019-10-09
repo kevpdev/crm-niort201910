83544696d3206bf7615b9a4ac6ea9c52bf67ba63
kevpdev <prepontkevin@gmail.com>
2019-10-09 14:20:26 +0200

    Pipe exemple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { UiModule } from './ui/ui.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
@@ -18,7 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
     NgbModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'fr-FR' }], // traduction pipe fr
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableauComponent } from './components/tableau/tableau.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TotalPipe } from './pipes/total.pipe';
 
 
 
 @NgModule({
-  declarations: [TableauComponent, ButtonComponent],
-  exports: [TableauComponent, ButtonComponent], // export pour que le composant soit dispo dans les autres modules (componentName --export)
+  declarations: [TableauComponent, ButtonComponent, TotalPipe],
+  // export pour que le composant soit dispo dans les autres modules (componentName --export)
+  exports: [TableauComponent, ButtonComponent, TotalPipe],
   imports: [
     CommonModule
   ]
